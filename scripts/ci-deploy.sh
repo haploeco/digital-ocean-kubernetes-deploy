a28169d081fb8eed840fc78325b147e7fe1db8be
Nate Thompson <nate.thompson@digitalglobe.com>
2020-01-05 18:16:05 -0700

    Fixed another fat-fingered typo

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -11,7 +11,7 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # it's not possible to do in-place substitution so we need to save the output to another file
 # and overwrite the original with that one
 envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-mv ./kube/do-sample-deploymnet.yml.out ./kube/do-sample-deployment.yml
+mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
