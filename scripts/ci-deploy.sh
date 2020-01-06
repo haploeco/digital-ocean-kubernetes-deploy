4d9adbd3093ce59da7525b94c64e3a37a1345c98
Nate Thompson <nate.thompson@digitalglobe.com>
2020-01-05 18:14:59 -0700

    Fixed yaml/yml typo

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -10,7 +10,7 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way to envsubst to work on files is using input/ouput redirection,
 # it's not possible to do in-place substitution so we need to save the output to another file
 # and overwrite the original with that one
-envsubst <./kube/do-sample-deployment.yaml >./kube/do-sample-deployment.yml.out
+envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
 mv ./kube/do-sample-deploymnet.yml.out ./kube/do-sample-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
