77fc5d056777a83974212b0e6fabc9e19629c242
Nate Thompson <nate.thompson@digitalglobe.com>
2020-01-05 18:17:24 -0700

    Fixed yet ANOTHER fat-fingered typo

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -18,6 +18,6 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 ./kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
-  --certificate-authority-=cert.crt \
+  --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
   apply -f ./kube/
